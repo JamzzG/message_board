c36d8f281db4c921d4c1bd543c8dc25592e2acbb
James Grantham <granthamjames@yahoo.com>
2023-02-18 09:14:18 -0700

    Saved an early version of my css, wanting to customozie the bootstrap files I aalready added and include an occcasional annoying fly on the screen that will die when you hover over it with a mouse but didn't get far with internet issues.  Will complete in later saves.

--- config/settings.py
+++ config/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'pages',
     'posts',
+    'static',
+
 ]
 
 MIDDLEWARE = [
@@ -119,7 +121,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [str(BASE_DIR.joinpath("static"))]   #Every project will need this snippet
-STATIC_ROOT = str(BASE_DIR.joinpath("staticfiles")) #centralizes files we will use this later
+# STATIC_ROOT = str(BASE_DIR.joinpath("staticfiles")) #centralizes files we will use this later
+
+MEDIA_ROOT =  [os.path.join(BASE_DIR, "media"), ] 
+MEDIA_URL = '/media/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
